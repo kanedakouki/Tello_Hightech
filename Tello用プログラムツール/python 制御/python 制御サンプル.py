24627dd17a77906db0201dfaa4685bbbbe578955
zilconia <85538741+zilconia@users.noreply.github.com>
2022-05-17 14:28:33 +0900

    python 制御 サンプル or テスト 用の2種類のファイルを作成

---
+++ Tello用プログラムツール/python 制御/python 制御サンプル.py
@@ -0,0 +1,53 @@
+import threading 
+import socket
+
+
+# Telloからのレスポンス受信
+def udp_receiver():
+    count = 0
+    while True: 
+        try:
+            # クライアントからのメッセージの受信を受付開始(コネクションレス型)
+            data, server = sock.recvfrom(1518)
+        except Exception:
+            print ('\nExit . . .\n')
+            break
+
+# Tello側のローカルIPアドレス(デフォルト)、宛先ポート番号(コマンドモード用)
+TELLO_IP = '192.168.10.1'
+TELLO_PORT = 8889
+TELLO_ADDRESS = (TELLO_IP, TELLO_PORT)
+
+# UDP通信ソケットの作成
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# 自ホストで使用するIPアドレスとポート番号を設定
+sock.bind(('', TELLO_PORT))
+
+# 受信用スレッドの作成
+thread  = threading.Thread(target=udp_receiver)
+thread.daemon = True
+thread .start()
+
+while True: 
+
+    try:
+        msg = input("")
+
+        # メッセージがなければ何もしない
+        if not msg:
+            break  
+
+        # 「q」でソケット通信終了
+        if 'q' in msg:
+            print ('QUIT...')
+            sock.close()  
+            break
+
+        # データを送信
+        msg = msg.encode(encoding="utf-8") 
+        sent = sock.sendto(msg, TELLO_ADDRESS)
+
+    except KeyboardInterrupt:
+        sock.close()  
+        break
--- Tello用プログラムツール/python 制御/python 制御テスト用.py
+++ Tello用プログラムツール/python 制御/python 制御テスト用.py
@@ -1,52 +1,22 @@
-﻿import threading 
-import socket
+﻿import socket as sk
 
-# Telloからのレスポンス受信
-def udp_receiver():
-    count = 0
-    while True: 
-        try:
-            # クライアントからのメッセージの受信を受付開始(コネクションレス型)
-            data, server = sock.recvfrom(1518)
-        except Exception:
-            print ('\nExit . . .\n')
-            break
+# 送信側アドレス設定
+#              (IP,ポート番号)
+UserAddr = ('127.0.0.1',11111)
 
-# Tello側のローカルIPアドレス(デフォルト)、宛先ポート番号(コマンドモード用)
-TELLO_IP = '192.168.10.1'
-TELLO_PORT = 8889
-TELLO_ADDRESS = (TELLO_IP, TELLO_PORT)
+# 受信側アドレス設定
+#                 (IP,ポート番号)
+TelloAddr = ('192.168.10.1',8889)
 
-# UDP通信ソケットの作成
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# ソケット作成
+udpSock = sk.socket(sk.AF_INET,sk.SOCK_DGRAM)
+# 送信側アドレスにソケットを設定
 
-# 自ホストで使用するIPアドレスとポート番号を設定
-sock.bind(('', TELLO_PORT))
+# 送信データ作成
+data = ''
+# エンコード変換
+data = data.encode('utf-8')
 
-# 受信用スレッドの作成
-thread  = threading.Thread(target=udp_receiver)
-thread.daemon = True
-thread .start()
-
-while True: 
-
-    try:
-        msg = input("")
-
-        # メッセージがなければ何もしない
-        if not msg:
-            break  
-
-        # 「q」でソケット通信終了
-        if 'q' in msg:
-            print ('QUIT...')
-            sock.close()  
-            break
-
-        # データを送信
-        msg = msg.encode(encoding="utf-8") 
-        sent = sock.sendto(msg, TELLO_ADDRESS)
-
-    except KeyboardInterrupt:
-        sock.close()  
-        break
+udpSock.bind(UserAddr)
+# UDP通信の準備
+sock=sk.socket(sk.AF_INET,sk.SOCK_DGRAM)
